d3d613ba20ee8ac116d0e4727be5a91b4d260ca4
Name <123@ya.ru>
2023-01-19 06:26:50 +0300

    Stateless Widget и его жизненный цикл

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.indigo,
-      appBar: AppBar(
-        title: Text("My first app"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          "this is my Homepage",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
+  runApp(myFirstApp());
+}
+
+class myFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("My first App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  LinearProgressIndicator(value: 23),
+                  Text(
+                    '23%',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Text(
+                    'Press button to download',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  )
+                ],
+              )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
         ),
       ),
-    ),
-  )); //Нужен для создания виджета, который мы создаем и передаем в качестве параметра, по сути это то, что мы видим, когда запускаем приложение
+    );
+  }
 }
